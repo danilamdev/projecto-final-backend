90cc3129de2292d34fc061e3e1dd32edbb23add5
daniel lambarri <dalambarri@live.com>
2022-04-24 17:27:36 -0300

    comienzo mocks y rutas

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -5,6 +5,7 @@ import ListOfProducts from "../../components/listofProducts";
 import Form from "../../components/form";
 import Carrito from "../../components/carrito";
 import Chat from "../../components/chat";
+import { createProducts } from "../../services/createMockProducts";
 
 export default function Home() {
   const { productos, setProductos } = useContext(productContext);
@@ -13,6 +14,11 @@ export default function Home() {
     getProductos().then((productos) => setProductos(productos));
   }, [setProductos]);
 
+  const showMockProducts = async () => {
+    const mockProductos = await createProducts();
+    console.log(mockProductos);
+  };
+
   return (
     <>
       <Chat />
@@ -20,15 +26,21 @@ export default function Home() {
       <h1 className="text-4xl text-center font-bold w-3/4 m-auto py-24 md:w-[550px]">
         Ecomerce CoderHouse BACKEND
       </h1>
-      {productos.length === 0 
-        ? (<div className="w-full h-96 border grid place-content-center">
+      <div className="max-w-md mx-auto mb-8">
+        <button
+          className="border border-blue-400 mx-auto block p-2"
+          onClick={showMockProducts}
+        >
+          mostrar productos
+        </button>
+      </div>
+      {productos.length === 0 ? (
+        <div className="w-full h-96 border grid place-content-center">
           <h1 className="text-5xl text-pink-400 font-bold">loading...</h1>
-        </div>)
-        : (
-          <ListOfProducts productos={productos} />
-
-        )
-      }
+        </div>
+      ) : (
+        <ListOfProducts productos={productos} />
+      )}
       <Form />
     </>
   );
